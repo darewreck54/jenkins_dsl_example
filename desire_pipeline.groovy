f77a477f7a573395114c753573439799401afe17
Derek Hang <dhang@snapchat.com>
2021-06-27 12:21:04 -0700

    update

--- desire_pipeline.groovy
+++ desire_pipeline.groovy
@@ -1,7 +1,7 @@
 def gitUrl = 'https://github.com/alicommit-malp/jenkins_dsl_example'
 
 for(i in 0..3) {
-    job('test-job-${i}') {
+    job('test-job-' + i) {
         scm {
             git(gitUrl)
         }
@@ -9,11 +9,12 @@ for(i in 0..3) {
             scm('*/15 * * * *')
         }
         steps {
-            shell(
+            shell ```
                 //sh "pip install -r $WORKSPACE/python/requirements.txt"
                 //sh "python $WORKSPACE/python/main.py"
                 "echo 'run after Maven'"
-            )
+
+            ```
             
         }
     }
