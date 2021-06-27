6a754a19cb0836a7f1c91e0b35b85cf8c2770b54
Derek Hang <dhang@snapchat.com>
2021-06-27 14:05:43 -0700

    push

--- desire_pipeline.groovy
+++ desire_pipeline.groovy
@@ -2,6 +2,27 @@ def gitUrl = 'https://github.com/alicommit-malp/jenkins_dsl_example'
 
 for(i in 0..3) {
     job('test-job-' + i) {
+        /*
+        configure {
+            def gblpass = it / 'buildWrappers'  / 'EnvInjectPasswordWrapper'  / 'passwordEntries' /
+            injectGlobalPasswords(true)
+            gblpass << 
+            'EnvInjectPasswordEntry' {
+                name("DEV_PWD")
+                'password' "test123"
+            }
+        }
+        */
+        wrappers {
+            credentialsBinding {
+            usernamePassword('USER','MYPASSWORD', 'myCredentialId')
+            }
+        }
+        environmentVariables {
+            env('ONE', '1')
+            propertiesFile('env.properties')
+            keepBuildVariables(true)
+        }
         scm {
             git(gitUrl)
         }
@@ -9,7 +30,11 @@ for(i in 0..3) {
             scm('*/15 * * * *')
         }
         steps {
-            shell "echo 'run after Maven'"
+            shell {
+                "echo 'run after Maven'"
+                echo 'my password is : ' \$MYPASSWORD
+
+            }
         }
     }
 }
